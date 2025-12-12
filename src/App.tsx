2cd78e90db85c68203c8c20f7bdea4c318e8c12a
kaikegmoraes-arch <kaikegmoraes@gmail.com>
2025-12-12 11:16:18 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import Login from "./pages/Login";
+import Admin from "./pages/Admin";
+import NotFound from "./pages/NotFound";
 import Suporte from "./pages/Suporte";
 import Orcamento from "./pages/Orcamento";
-import Admin from "./pages/Admin";
-import Login from "./pages/Login";
-import NotFound from "./pages/NotFound";
+import TesteSupabase from "./pages/teste-supabase";
 
-const queryClient = new QueryClient();
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/suporte" element={<Suporte />} />
+        <Route path="/orcamento" element={<Orcamento />} />
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/suporte" element={<Suporte />} />
-          <Route path="/orcamento" element={<Orcamento />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+        {/* 🔥 ROTA DE TESTE DO SUPABASE */}
+        <Route path="/teste-supabase" element={<TesteSupabase />} />
+
+        {/* 404 */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
