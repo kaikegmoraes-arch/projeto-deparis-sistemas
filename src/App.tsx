36e2c12513309f2e1839c3341ef49a0b7fcb25e5
kaikegmoraes-arch <kaikegmoraes@gmail.com>
2025-12-12 14:37:43 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,3 @@ import Admin from "./pages/Admin";
 import NotFound from "./pages/NotFound";
 import Suporte from "./pages/Suporte";
 import Orcamento from "./pages/Orcamento";
-import TesteSupabase from "./pages/teste-supabase";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/suporte" element={<Suporte />} />
-        <Route path="/orcamento" element={<Orcamento />} />
-
-        {/* 🔥 ROTA DE TESTE DO SUPABASE */}
-        <Route path="/teste-supabase" element={<TesteSupabase />} />
-
-        {/* 404 */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
